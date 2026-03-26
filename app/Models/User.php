110329dd01524bfabbccec959e3363cac4b3e65b
ayouberrak <ayouberrak30@gmail.com>
2026-03-26 10:12:45 +0100

    refactor(models): update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,45 +5,48 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function courses(){
+        return $this->hasMany(Course::class,'teacher_id');
+    }
+
+    public function enrollments(){
+        return $this->hasMany(Enrollment::class);
+    }
+
+
+    public function favorites(){
+        return $this->belongsToMany(Course::class,'favorites');
+    }
+
+    public function domains(){
+        return $this->belongsToMany(Domain::class, 'domain_users');
     }
 }
