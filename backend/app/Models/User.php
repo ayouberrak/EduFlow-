7dab74f8e2d76fb679bca54bbf99680914462929
ayouberrak <ayouberrak30@gmail.com>
2026-03-29 15:50:35 +0100

    feat(backend): add JWT support and domains relationship to User model

---
+++ backend/app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+    public function courses(){
+        return $this->hasMany(Course::class,'teacher_id');
+    }
+
+    public function enrollments(){
+        return $this->hasMany(Enrollment::class);
+    }
+
+
+    public function favorites(){
+        return $this->belongsToMany(Course::class,'favorites');
+    }
+
+    public function domains(){
+        return $this->belongsToMany(Domain::class, 'domain_users');
+    }
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+}
